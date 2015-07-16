dfade33cf80ca0f5a812775d87bc4c5ec1df8fac
KKolluri <k.kedar@gmail.com>
2015-07-16 18:24:02 -0400

    more in the playground

--- tests/struct_arbit.cpp
+++ tests/struct_arbit.cpp
@@ -4,6 +4,7 @@
 #include <set>
 #include <tuple>
 #include <map>
+#include <typeinfo>
 
 typedef struct teststruct{
   int test1;
@@ -61,9 +62,56 @@ void execute_one()
   map_double[std::make_tuple(1,2)] = 12.434;
 }
 
+void test_tuplesinmap()
+{
+  std::map< std::tuple<int, int>, int> testmap1;
+  auto abcd = std::make_tuple(1, 3);
+  testmap1[abcd] = 1;
+  std::cout << testmap1[abcd]<<"\n";
+  abcd = std::make_tuple(3, 1);
+  testmap1[abcd]= 10;
+  std::cout << testmap1[abcd]<<"\n";
+}
+
+void test_loop_map()
+{
+  //create a map and fill it
+  std::map<int, test> looptestmap;
+
+  for(int i =0;i<10;i++)
+  {
+    test testtest;
+    testtest.test1 = i*i;
+    testtest.test2  = i*i*i;
+    looptestmap[i] = testtest;
+  }
+  // retrieve it back again
+
+  for(auto iter = looptestmap.begin(); iter != looptestmap.end(); ++iter)
+  {
+    test localtest = iter->second;
+    std::cout << iter->first<<" "<<localtest.test1 << " "<< localtest.test2<<" \n";
+  }
+
+}
+void test_tuple_vector()
+{
+  std::vector< std::tuple<int, int> > tuplevector;
+  tuplevector.push_back(std::make_tuple(1, 1));
+  tuplevector.push_back(std::make_tuple(2, 2));
+  tuplevector.push_back(std::make_tuple(3, 2));
+  tuplevector.push_back(std::make_tuple(4, 2));
+
+  for(auto iter = tuplevector.begin(); iter !=tuplevector.end(); ++iter)
+  {
+    std::cout << typeid(iter).name()<<"\t"<< typeid(*iter).name()<<"\n";
+    std::cout << std::get<0>(*iter)<<"\n";
+  }
+}
 int main(int argc, char *argv[])
 {
-  toptest tt2 = filltoptest();
-  std::cout << tt2.test0<<"\n";
+  //test_loop_map();
+  test_tuple_vector();
+
   return 0;
 }
