c6cd68a320772503cb567993191ab23375c8fcc7
KKolluri <k.kedar@gmail.com>
2015-07-16 00:35:53 -0400

    some tests and modifs to gitignore

--- tests/struct_arbit.cpp
+++ tests/struct_arbit.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <vector>
 #include <set>
+#include <tuple>
+#include <map>
 
 typedef struct teststruct{
   int test1;
@@ -10,7 +12,30 @@ typedef struct teststruct{
   std::vector<int> test4;
 };
 
-int main(int argc, char *argv[])
+typedef struct test{
+  int test1;
+  int test2;
+};
+
+typedef struct toptest{
+  int test0;
+  std::map<int, test> testmap;
+};
+
+toptest filltoptest()
+{
+  int n=100;
+  toptest tt1;
+  tt1.test0=100;
+  for (int i=0;i<100;i++)
+  {
+    tt1.testmap[i].test1 = i*i;
+    tt1.testmap[i].test2 = i*i*i;
+  }
+  return tt1;
+}
+
+void execute_one()
 {
   teststruct abcd;
   abcd.test1 = 2;
@@ -29,5 +54,16 @@ int main(int argc, char *argv[])
   for (int i = 0 ; i  < 5; i++)
     std::cout << abcd.test3[i]<<" ";
   std::cout << "\n";
+
+  std::map<std::tuple<int, int>, double> map_double;
+  auto tpl = std::make_tuple(1,1);
+  map_double[tpl] = 23.2;
+  map_double[std::make_tuple(1,2)] = 12.434;
+}
+
+int main(int argc, char *argv[])
+{
+  toptest tt2 = filltoptest();
+  std::cout << tt2.test0<<"\n";
   return 0;
 }
